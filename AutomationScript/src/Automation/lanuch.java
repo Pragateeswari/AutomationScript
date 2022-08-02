975e329f33d457fd0e1b32b17c9ccf285b3b56d2
pragateeswari.s <pragateeswari.s@QT-INBG-LT0354.QUALITEST.LOCAL>
2022-08-02 12:48:58 +0530

    launch

--- AutomationScript/src/Automation/lanuch.java
+++ AutomationScript/src/Automation/lanuch.java
@@ -1,13 +1,22 @@
 package Automation;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class lanuch {
-WebDriver driver;
-@Test public void Lanching_the Browser()
+ WebDriver driver;
+@Test 
+public void Lanching_the Browser()
 {
- System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
- driver = new ChromeDriver();
- driver.get("https://mobileworld.azurewebsites.net");
- driver.manage().window().maximize();	
+ //System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+ 
+  WebDriverManager.chromedriver().setup();
+  driver = new ChromeDriver();
+  driver.get("https://mobileworld.azurewebsites.net");
+  driver.manage().window().maximize();	
 }
 
 }
